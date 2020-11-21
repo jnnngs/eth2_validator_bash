34fbeec881975cc1399ba0c8e5d4716f7d6c0efd
Paul Jennings <4107228+jnnngs@users.noreply.github.com>
2020-11-21 14:32:16 +0000

    Update eth2_harden.sh

--- eth2_harden.sh
+++ eth2_harden.sh
@@ -946,6 +946,7 @@ function install_complete() {
 function display_banner() {
 
     echo -e -n "${lightcyan}"
+    apt-get update
     apt-get -qqy -o=Dpkg::Use-Pty=0 -o=Acquire::ForceIPv4=true install figlet | tee -a "$LOGFILE"
     figlet jnnngs -f small
     figlet harden OS -f small
